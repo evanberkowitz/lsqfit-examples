a34da0b5dcdf39d8c4d27d7d48e23b6bc7710858
Evan Berkowitz <git@evanberkowitz.com>
2020-07-20 17:35:35 -0400

    Replace points with pixels for clarity.

--- ellipse.py
+++ ellipse.py
@@ -119,9 +119,9 @@ result = np.array([[x.mean for x in r] for r in result])
 
 fig, ax = plt.subplots()
 for sigma in [0,1,2]:
-    ax.plot(*(result+sigma*sdev), linestyle='none', marker='.')
-    ax.plot(*(result-sigma*sdev), linestyle='none', marker='.', color=ax.lines[-1].get_color())
-ax.plot(*exact, linestyle='none', marker='.', color='black')
+    ax.plot(*(result+sigma*sdev), linestyle='none', marker=',')
+    ax.plot(*(result-sigma*sdev), linestyle='none', marker=',', color=ax.lines[-1].get_color())
+ax.plot(*exact, linestyle='none', marker=',', color='black')
 ax.errorbar([x.mean for x in X], [y.mean for y in Y], xerr = [x.sdev for x in X], yerr = [y.sdev for y in Y],
         linestyle='none',
         color='black',
